213ddad31575bf936c7be48bb773c169ab521ea9
gitlab@srinivas.gs <code@srinivas.gs>
2018-10-16 14:29:13 -0400

    wrote startWorker for finer grained control

--- @psychopomp/simulate.m
+++ @psychopomp/simulate.m
@@ -25,27 +25,17 @@ function simulate(self)
 	for i = 1:length(self.clusters)
 		if strcmp(self.clusters(i).Name,'local')
 
-			% check that every job has the correct hash
-			% disp('Checking that every job has the same hash...')
-			% do_folder = [self.psychopomp_folder filesep 'do' filesep ];
-			% allfiles = dir([do_folder '*.ppp']);
-			% for j = 1:length(allfiles)
-			% 	textbar(j,length(allfiles))
-			% 	m = matfile(joinPath(allfiles(j).folder,allfiles(j).name));
-			% 	assert(strcmp(self.xolotl_hash,m.xhash),'At least one job didnt match the hash of the currently configured Xolotl object')
-			% end
-
 			self.sim_start_time = now;
 
 			if self.verbosity
 				disp('Starting workers...')
 			end
 
-			for j = self.num_workers:-1:1
-				F(j) = parfeval(@self.simulate_core,0,j,Inf);
-				textbar(self.num_workers - j + 1,self.num_workers)
+			for j = 1:self.num_workers
+				self.startWorker();
+				textbar(j, self.num_workers)
 			end
-			self.workers = F;
+
 
 		else
 			% it's a remote cluster. ask the remote (nicely)
